a2ef77bb6b699ac3ca5680f96d5f8da7bf59dc7a
asher228866-arch <asher228866@gmail.com>
2026-05-29 15:03:43 +0300

    Update test_buckling.py

--- test_buckling.py
+++ test_buckling.py
@@ -1,20 +1,12 @@
 import numpy as np
 from scipy import optimize
+def column_stress_error(P,L,E,A,r,c,e,sigma_allow):
+    #פרמטרים לדוגמא
+    #A,E,L,r,e,c =5000,200000,3000,50,20,100
+    #נוסחאת הסקנט
+    sec_term = 1 / np.cos((L/(2*r)) * np.sqrt(P/(E*A)))
+    sigma_max - sigma_allow
 
-def column_stress_error(P, L, E, A, r, c, e, sigma_allow):
-    # חישוב האיבר שבתוך הקוסינוס (ברדיאנים)
-    cos_argument = (L / (2 * r)) * np.sqrt(P / (E * A))
-    
-    # נוסחת הסקנט למציאת המאמץ המקסימלי
-    sigma_max = (P / A) * (1 + (e * c / r**2) * (1 / np.cos(cos_argument)))
-    
-    # מחזירים את ההפרש - ניוטון-רפסון ישאף להביא אותו ל-0
-    return sigma_max - sigma_allow
-
-def find_critical_load(L, E, A, r, c, e, sigma_allow):
-    # נקודת ניחוש ראשונית (למשל 100,000 ניוטון או כל ערך הגיוני אחר)
-    initial_guess = 500000 
-    
-    # תיקון: שינוי ה-I ל-L כפי שהוגדר בפונקציה
-    p_critical = optimize.newton(lambda P: column_stress_error(P, L, E, A, r, c, e, sigma_allow), initial_guess)     
-    return p_critical
+def find_critical_load(L,E,A,r,c,e, sigma_allow):
+  p_critical = optimize.newton(lambda P: column_stress_error(P,I,E,A,r,c,e, sigma_allow),500000)    
+  return p_critical
