0867093c6101883f19eaba236283a288e5b09236
asher228866-arch <asher228866@gmail.com>
2026-05-29 15:22:01 +0300

    Update test_buckling.py

--- test_buckling.py
+++ test_buckling.py
@@ -1,12 +1,12 @@
 import numpy as np
 from scipy import optimize
-def column_stress_error(P,L,E,A,r,c,e,sigma_allow):
+def column_stress_error(P,L,E,A,r,c,e, sigma_allow):
     #פרמטרים לדוגמא
-    #A,E,L,r,e,c =5000,200000,3000,50,20,100
+    #A,E,L,r,e,c = 5000, 200000, 3000, 50, 20, 100
     #נוסחאת הסקנט
     sec_term = 1 / np.cos((L/(2*r)) * np.sqrt(P/(E*A)))
     sigma_max - sigma_allow
 
-def find_critical_load(L,E,A,r,c,e, sigma_allow):
-  p_critical = optimize.newton(lambda P: column_stress_error(P,I,E,A,r,c,e, sigma_allow),500000)    
+def find_critical_load(L, E, A, r, c, e, sigma_allow):
+  p_critical = optimize.newton(lambda P: column_stress_error(P, L, E, A, r, c, e, sigma_allow), 500000)    
   return p_critical
