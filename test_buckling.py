5624670806f983f2a4d154e798b335bbab8570f5
asher228866-arch <asher228866@gmail.com>
2026-05-29 15:54:42 +0300

    Update test_buckling.py

--- test_buckling.py
+++ test_buckling.py
@@ -5,7 +5,7 @@ def column_stress_error(P, L, E, A, r, c, e, sigma_allow):
     #A,E,L,r,e,c = 5000, 200000, 3000, 50, 20, 100
     #נוסחאת הסקנט
     sec_term = 1 / np.cos((L/(2*r)) * np.sqrt(P/(E*A)))
-    sigma_max = (P / A) * (1 + (e * c / r**2) * sec_term)
+    sigma_max = (P / A) * (1 + (e*c/r**2) * sec_term)
     return sigma_max - sigma_allow
     
 def find_critical_load(L, E, A, r, c, e, sigma_allow):
