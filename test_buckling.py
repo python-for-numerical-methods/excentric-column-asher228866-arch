7db9ed8bc15fc07a990fda75b5840718613ae992
asher228866-arch <asher228866@gmail.com>
2026-06-21 13:29:56 +0300

    Update test_buckling.py

--- test_buckling.py
+++ test_buckling.py
@@ -5,7 +5,7 @@ def column_stress_error(P, L, E, A, r, c, e, sigma_allow):
     # פרמטרים לדוגמה
     #A, E, L, r, e, c = 5000, 200000, 3000, 50, 20, 100
     # נוסחת הסקנט #
-    sec_term = 1 / np.cos(((L/(2*r)) * np.sqrt(P/(E*A))))
+        sec_term = 1 / np.cos(((L/(2*r)) * np.sqrt(P/(E*A))))
     sigma_max = (P/A) * (1 + (e*c/r**2) * sec_term)
     return sigma_max - sigma_allow
 
